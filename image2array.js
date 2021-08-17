1e711e057810b58ec82acca39379feee9fba0e61
Tomas Tulka <tomas.tulka@gmail.com>
2021-08-17 09:47:23 +0200

    use pngjs

--- image2array.js
+++ image2array.js
@@ -1,23 +1,25 @@
-const image = require("get-image-data");
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
 
 const filename = process.argv[2] || "./image.png";
- 
-image(filename, function (err, info) {
-  const data = info.data;
-  // const width = info.width;
-  // const height = info.height;
 
-  process.stdout.write("const image: u8[] = [");
+const file = fs.readFileSync(filename);
+const png = PNG.sync.read(file);
  
-  for (var i = 0, l = data.length; i < l; i += 4) {
-    const r = data[i];
-    const g = data[i + 1];
-    const b = data[i + 2];
-    const a = data[i + 3];
-   
-    process.stdout.write(`${r}, ${g}, ${b}, ${a}, `);
-  }
+const data = png.data;
+// const width = png.width;
+// const height = png.height;
 
-  process.stdout.write("];\n");
-  process.stdout.write("export default image;\n");
-})
+process.stdout.write("const image: u8[] = [");
+
+for (var i = 0, l = data.length; i < l; i += 4) {
+  const r = data[i];
+  const g = data[i + 1];
+  const b = data[i + 2];
+  const a = data[i + 3];
+  
+  process.stdout.write(`${r}, ${g}, ${b}, ${a}, `);
+}
+
+process.stdout.write("];\n");
+process.stdout.write("export default image;\n");
